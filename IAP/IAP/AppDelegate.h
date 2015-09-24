aea932d0c6007b5f4d9bead9eed7858b6d1d32d9
Dylan <3664132@163.com>
2015-09-24 12:13:38 +0800

    Commit Files
    Commit Files

---
+++ IAP/IAP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IAP
+//
+//  Created by Dylan on 15/9/24.
+//  Copyright (c) 2015年 Dylan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IAP/IAP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IAP
+//
+//  Created by Dylan on 15/9/24.
+//  Copyright (c) 2015年 Dylan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
